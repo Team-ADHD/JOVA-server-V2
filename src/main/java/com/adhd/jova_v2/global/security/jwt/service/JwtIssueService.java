7f36364a9d6d2f9dcd9340be835763bcc85ccf42
snowykte0426 <snowykte0426@naver.com>
2025-01-13 11:29:06 +0900

    refactor: UUID 사용 설정 및 라이브러리 버전 상승에 따른 메서드 사용법 변경
    set을 이용하여 JWT를 빌드하던 방법에서 claim으로 구성하는 방안으로 수정하였습니다
    
    Resolves: #10

--- src/main/java/com/adhd/jova_v2/global/security/jwt/service/JwtIssueService.java
+++ src/main/java/com/adhd/jova_v2/global/security/jwt/service/JwtIssueService.java
@@ -3,8 +3,10 @@ package com.adhd.jova_v2.global.security.jwt.service;
 import com.adhd.jova_v2.global.security.dto.TokenDto;
 import com.adhd.jova_v2.global.security.enums.role.UserRole;
 
-public interface JwtIssueService {
-    TokenDto issueAccessToken(String userId, UserRole role);
+import java.util.UUID;
 
-    TokenDto issueRefreshToken(String userId);
+public interface JwtIssueService {
+    TokenDto issueAccessToken(UUID userId, UserRole role);
+
+    TokenDto issueRefreshToken(UUID userId);
 }
--- src/main/java/com/adhd/jova_v2/global/security/jwt/service/impl/JwtIssueServiceImpl.java
+++ src/main/java/com/adhd/jova_v2/global/security/jwt/service/impl/JwtIssueServiceImpl.java
@@ -6,16 +6,21 @@ import com.adhd.jova_v2.global.security.exception.RefreshTokenSaveException;
 import com.adhd.jova_v2.global.security.jwt.component.SaveRefreshToken;
 import com.adhd.jova_v2.global.security.jwt.service.JwtIssueService;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
+import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class JwtIssueServiceImpl implements JwtIssueService {
@@ -27,34 +32,43 @@ public class JwtIssueServiceImpl implements JwtIssueService {
     private Long refreshTokenExpiration;
     @Value("${spring.security.jwt.secret}")
     private String secretKey;
-    private final SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    private SecretKey key;
+
+    @PostConstruct
+    public void init() {
+        key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        log.info("Key Initialized successfully");
+    }
 
     @Override
-    public TokenDto issueAccessToken(String userId, UserRole role) {
+    public TokenDto issueAccessToken(UUID userId, UserRole role) {
         LocalDateTime expiration = LocalDateTime.now().plusSeconds(accessTokenExpiration);
+
         String accessToken = Jwts.builder()
-                .setSubject(userId)
-                .setIssuedAt(Date.from(LocalDateTime.now().atZone(java.time.ZoneId.systemDefault()).toInstant()))
-                .setExpiration(Date.from(expiration.atZone(java.time.ZoneId.systemDefault()).toInstant()))
+                .claim("sub", userId.toString())
                 .claim("role", role)
-                .signWith(key, SignatureAlgorithm.HS256)
+                .claim("iat", Date.from(Instant.now()))
+                .claim("exp", Date.from(expiration.atZone(ZoneId.systemDefault()).toInstant()))
+                .signWith(key)
                 .compact();
+
         return new TokenDto(accessToken, expiration);
     }
 
     @Override
-    public TokenDto issueRefreshToken(String userId) {
+    public TokenDto issueRefreshToken(UUID userId) {
         LocalDateTime expiration = LocalDateTime.now().plusSeconds(refreshTokenExpiration);
         String refreshToken = Jwts.builder()
-                .setSubject(userId)
-                .setIssuedAt(Date.from(LocalDateTime.now().atZone(java.time.ZoneId.systemDefault()).toInstant()))
-                .setExpiration(Date.from(expiration.atZone(java.time.ZoneId.systemDefault()).toInstant()))
-                .signWith(key, SignatureAlgorithm.HS256)
+                .claim("sub", userId.toString())
+                .claim("iat", Date.from(Instant.now()))
+                .claim("exp", Date.from(expiration.atZone(ZoneId.systemDefault()).toInstant()))
+                .signWith(key)
                 .compact();
         TokenDto tokenDto = new TokenDto(refreshToken, expiration);
-        if (!saveRefreshToken.save(tokenDto)) {
+        if (!saveRefreshToken.save(tokenDto, userId)) {
             throw new RefreshTokenSaveException("Refresh token save failed");
         }
+
         return tokenDto;
     }
 }
