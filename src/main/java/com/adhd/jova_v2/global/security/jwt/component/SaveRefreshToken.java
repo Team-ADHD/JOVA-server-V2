cbf90b51039a6ce0990511fc16b283298b6c24c4
snowykte0426 <snowykte0426@naver.com>
2025-01-13 11:35:17 +0900

    refactor: ``RefreshTokenRedisRepository`` 사용 중지 및 ``RedisUtil`` 클래스 사용
    RedisTemplate를 사용하여 I/O를 수행하도록 리팩터링 하였습니다
    
    Resolves: #10

--- src/main/java/com/adhd/jova_v2/global/security/jwt/component/SaveRefreshToken.java
+++ src/main/java/com/adhd/jova_v2/global/security/jwt/component/SaveRefreshToken.java
@@ -1,28 +1,25 @@
 package com.adhd.jova_v2.global.security.jwt.component;
 
+import com.adhd.jova_v2.global.redis.util.RedisUtil;
 import com.adhd.jova_v2.global.security.dto.TokenDto;
-import com.adhd.jova_v2.global.security.jwt.entity.RefreshToken;
-import com.adhd.jova_v2.global.security.jwt.repository.RefreshTokenRedisRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class SaveRefreshToken {
 
-    private final RefreshTokenRedisRepository refreshTokenRedisRepository;
+    private final RedisUtil redisUtil;
 
-    public boolean save(TokenDto tokenDto) {
+    public boolean save(TokenDto tokenDto, UUID userId) {
         try {
-            refreshTokenRedisRepository.save(
-                    RefreshToken.builder()
-                            .refreshToken(tokenDto.getToken()).
-                            expiredAt(tokenDto.getExpiredAt().toString())
-                            .build()
-            );
+            redisUtil.set(tokenDto.getToken(), userId, 604800, TimeUnit.SECONDS, false);
         } catch (DataAccessException e) {
             log.error("Failed to save refresh token due to Redis error: {}", e.getMessage(), e);
             return false;
