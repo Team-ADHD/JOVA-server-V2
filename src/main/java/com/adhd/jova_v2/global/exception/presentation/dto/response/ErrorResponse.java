7af8e8ce51eb2501dc2de1c87e98481d859bfa7b
snowykte0426 <snowykte0426@naver.com>
2025-01-14 17:58:56 +0900

    feat: ``@Getter`` 어노테이션 적용
    ``@Getter`` 어노테이션을 적용하여 Get 메서드를 추가하였습니다
    
    Resolves: #10

--- src/main/java/com/adhd/jova_v2/global/exception/presentation/dto/response/ErrorResponse.java
+++ src/main/java/com/adhd/jova_v2/global/exception/presentation/dto/response/ErrorResponse.java
@@ -2,10 +2,12 @@ package com.adhd.jova_v2.global.exception.presentation.dto.response;
 
 import com.adhd.jova_v2.global.exception.presentation.enums.ErrorType;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class ErrorResponse {
     ErrorType errorType;
     String message;
