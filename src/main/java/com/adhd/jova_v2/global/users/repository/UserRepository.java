bf7b4ad1a61b78e58bc28ef89ad285c923f3a52b
snowykte0426 <snowykte0426@naver.com>
2025-01-14 15:36:12 +0900

    feat: ``existsByEmail``,``findByEmail`` 메서드 추가
    메서드를 추가하였습니다
    
    Resolves: #10

--- src/main/java/com/adhd/jova_v2/global/users/repository/UserRepository.java
+++ src/main/java/com/adhd/jova_v2/global/users/repository/UserRepository.java
@@ -4,5 +4,11 @@ import com.adhd.jova_v2.global.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> { }
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+}
