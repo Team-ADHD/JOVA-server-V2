6c6dfe96e072425426312f01343ce71951fc87a1
snowykte0426 <snowykte0426@naver.com>
2025-01-14 17:58:21 +0900

    chore: CORS 허용 출처 환경변수 적용
    좀 더 유연하게 CORS 설정이 가능하도록 환경변수를 적용하는 방안으로 선회하였습니다
    
    Resolves: #10

--- src/main/java/com/adhd/jova_v2/global/security/config/CorsConfig.java
+++ src/main/java/com/adhd/jova_v2/global/security/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.adhd.jova_v2.global.security.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,10 +13,14 @@ import java.util.List;
 @Configuration
 @RequiredArgsConstructor
 public class CorsConfig {
+
+    @Value("${spring.security.cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public CorsConfigurationSource configureCors() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(allowedOrigins.split(",")));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
