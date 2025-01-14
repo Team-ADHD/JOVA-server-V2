22930bae98717aff1f3fed5838fd6573d8f0513c
snowykte0426 <snowykte0426@naver.com>
2025-01-14 15:34:06 +0900

    feat: ``SignInService`` 메서드 실제 구현
    실제 로그인 비즈니스 로직을 구현하였습니다
    
    Resolves: #10

--- src/main/java/com/adhd/jova_v2/domain/auth/service/impl/SignInServiceImpl.java
+++ src/main/java/com/adhd/jova_v2/domain/auth/service/impl/SignInServiceImpl.java
@@ -3,21 +3,36 @@ package com.adhd.jova_v2.domain.auth.service.impl;
 import com.adhd.jova_v2.domain.auth.presentation.dto.request.SignInRequest;
 import com.adhd.jova_v2.domain.auth.presentation.dto.response.SignInResponse;
 import com.adhd.jova_v2.domain.auth.service.SignInService;
-import com.adhd.jova_v2.global.security.enums.role.UserRole;
+import com.adhd.jova_v2.global.security.dto.TokenDto;
 import com.adhd.jova_v2.global.security.jwt.service.JwtIssueService;
+import com.adhd.jova_v2.global.users.component.FindUser;
+import com.adhd.jova_v2.global.users.dto.UserDto;
+import com.adhd.jova_v2.global.users.exception.UserNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
 public class SignInServiceImpl implements SignInService {
 
     private final JwtIssueService jwtIssueService;
+    private final FindUser findUser;
 
     @Override
     public SignInResponse execute(SignInRequest request) {
-         // 아직 구현 안됨(임시로 넣어둔 값)
+        if (findUser.isExistUser(request.getEmail())) {
+            UserDto user = findUser.findUserByEmail(request.getEmail());
+            TokenDto accessToken = jwtIssueService.issueAccessToken(user.getUuid(), user.getRole());
+            TokenDto refreshToken = jwtIssueService.issueRefreshToken(user.getUuid());
+            return SignInResponse.builder()
+                    .accessToken(accessToken.getToken())
+                    .refreshToken(refreshToken.getToken())
+                    .accessTokenExpiredAt(accessToken.getExpiredAt())
+                    .refreshTokenExpiredAt(refreshToken.getExpiredAt())
+                    .role(user.getRole())
+                    .build();
+        } else {
+            throw new UserNotFoundException("Sign in failed. User not found.");
+        }
     }
 }
