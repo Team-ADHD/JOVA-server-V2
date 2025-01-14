011822454bd02651550b1eb29bdee03b222e66b2
snowykte0426 <snowykte0426@naver.com>
2025-01-14 15:33:37 +0900

    fix: ``@Repository`` 어노테이션 누락 문제 수정
    ``@Repository`` 어노테이션의 누락 문제를 해결하였습니다
    
    Resolves: #10

--- src/main/java/com/adhd/jova_v2/global/security/jwt/repository/RefreshTokenRedisRepository.java
+++ src/main/java/com/adhd/jova_v2/global/security/jwt/repository/RefreshTokenRedisRepository.java
@@ -2,6 +2,8 @@ package com.adhd.jova_v2.global.security.jwt.repository;
 
 import com.adhd.jova_v2.global.security.jwt.entity.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, String> {
 }
