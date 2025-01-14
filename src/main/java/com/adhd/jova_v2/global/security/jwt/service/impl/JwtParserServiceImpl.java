ed189e6610803fd86cc6cbfea08b32eadada9e47
snowykte0426 <snowykte0426@naver.com>
2025-01-14 15:31:04 +0900

    feat: ``JwtParserService`` 메서드 구현
    실제 구현이 되어있지 않던 메서드들을 구현하였습니다
    
    Resolves: #10

--- src/main/java/com/adhd/jova_v2/global/security/jwt/service/impl/JwtParserServiceImpl.java
+++ src/main/java/com/adhd/jova_v2/global/security/jwt/service/impl/JwtParserServiceImpl.java
@@ -2,25 +2,58 @@ package com.adhd.jova_v2.global.security.jwt.service.impl;
 
 import com.adhd.jova_v2.global.security.enums.role.UserRole;
 import com.adhd.jova_v2.global.security.jwt.service.JwtParserService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class JwtParserServiceImpl implements JwtParserService {
-    // TODO: Implement this class
+
+    @Value("${spring.security.jwt.secret}")
+    private String secretKey;
+    private SecretKey key;
+
+    @PostConstruct
+    public void init() {
+        key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        log.info("JwtParserService Initialized successfully");
+    }
+
     @Override
     public boolean validateToken(String token) {
-        return false;
+        try {
+            parserClaims(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            log.error("Token is expired: {}", e.getMessage());
+            return false;
+        } catch (Exception e) {
+            log.error("Token is invalid: {}", e.getMessage());
+            return false;
+        }
     }
 
     @Override
     public String extractUserEmail(String token) {
-        return "";
+        return parserClaims(token).getSubject();
     }
 
     @Override
     public UserRole extractUserRole(String token) {
-        return null;
+        return parserClaims(token).get("role", UserRole.class);
+    }
+
+    private Claims parserClaims(String token) {
+        return Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload();
     }
 }
