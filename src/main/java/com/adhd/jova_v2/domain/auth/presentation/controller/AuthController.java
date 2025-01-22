513ac25f7d82b371d6f28d1059efcee567a5c5ac
snowykte0426 <snowykte0426@naver.com>
2025-01-23 01:10:33 +0900

    fix: 어노테이션 누락 문제 해결
    Request Body로 사용하기 위한 어노테이션들이 누락되어 실제로 적용이 되지 않던 문제를 해결하였습니다
    
    Resolve: #10

--- src/main/java/com/adhd/jova_v2/domain/auth/presentation/controller/AuthController.java
+++ src/main/java/com/adhd/jova_v2/domain/auth/presentation/controller/AuthController.java
@@ -3,10 +3,12 @@ package com.adhd.jova_v2.domain.auth.presentation.controller;
 import com.adhd.jova_v2.domain.auth.presentation.dto.request.SignInRequest;
 import com.adhd.jova_v2.domain.auth.presentation.dto.response.SignInResponse;
 import com.adhd.jova_v2.domain.auth.service.SignInService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,7 @@ public class AuthController {
     private final SignInService signInService;
 
     @PostMapping("/signin")
-    public ResponseEntity<SignInResponse> signIn(SignInRequest request) {
+    public ResponseEntity<SignInResponse> signIn(@Valid @RequestBody SignInRequest request) {
         return ResponseEntity.status(HttpStatus.OK).body(signInService.execute(request));
     }
 }
