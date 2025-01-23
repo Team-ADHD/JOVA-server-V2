65b1ada933c7b54d6b3f9f9ca8045304170e32f8
snowykte0426 <snowykte0426@naver.com>
2025-01-23 18:08:20 +0900

    feat: ``POST /api/v2/auth/refresh`` 엔드포인트 정의
    JWT의 재발급을 처리하는 엔드포인트를 정의하였습니다
    
    Resolve: #10

--- src/main/java/com/adhd/jova_v2/domain/auth/presentation/controller/AuthController.java
+++ src/main/java/com/adhd/jova_v2/domain/auth/presentation/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.adhd.jova_v2.domain.auth.presentation.controller;
 
+import com.adhd.jova_v2.domain.auth.presentation.dto.request.RefreshRequest;
 import com.adhd.jova_v2.domain.auth.presentation.dto.request.SignInRequest;
-import com.adhd.jova_v2.domain.auth.presentation.dto.response.SignInResponse;
+import com.adhd.jova_v2.domain.auth.presentation.dto.response.TokenResponse;
+import com.adhd.jova_v2.domain.auth.service.RefreshService;
 import com.adhd.jova_v2.domain.auth.service.SignInService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,9 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final SignInService signInService;
+    private final RefreshService refreshService;
 
     @PostMapping("/signin")
-    public ResponseEntity<SignInResponse> signIn(@Valid @RequestBody SignInRequest request) {
+    public ResponseEntity<TokenResponse> signIn(@Valid @RequestBody SignInRequest request) {
         return ResponseEntity.status(HttpStatus.OK).body(signInService.execute(request));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<TokenResponse> refresh(@Valid @RequestBody RefreshRequest request) {
+        return ResponseEntity.status(HttpStatus.OK).body(refreshService.execute(request.getRefreshToken()));
     }
 }
