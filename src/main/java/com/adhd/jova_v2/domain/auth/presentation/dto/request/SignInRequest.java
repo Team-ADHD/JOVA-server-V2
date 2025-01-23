554e92a4d46ee7d27d1c8c7bec7e206dd09f9b7b
snowykte0426 <snowykte0426@naver.com>
2025-01-23 18:19:32 +0900

    refactor: 불필요 어노테이션 제거
    생성자로만 생성되는 클래스의 어노테이션이 불필요하다 판단되어 제거하였습니다
    
    Resolve: #10

--- src/main/java/com/adhd/jova_v2/domain/auth/presentation/dto/request/SignInRequest.java
+++ src/main/java/com/adhd/jova_v2/domain/auth/presentation/dto/request/SignInRequest.java
@@ -3,14 +3,12 @@ package com.adhd.jova_v2.domain.auth.presentation.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Builder
 public class SignInRequest {
     @Email
     @NotBlank
