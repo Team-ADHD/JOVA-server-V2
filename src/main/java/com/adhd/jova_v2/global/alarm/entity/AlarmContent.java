4a8732763b9e42e4912828dad9b341e2ed4660c5
snowykte0426 <snowykte0426@naver.com>
2025-01-29 15:48:28 +0900

    refactor: 필드 제약조건 추가 및 패키지 이동
    Entity의 일부라 판단하여 DTO 패키지에서 Entity 패키지로 이동하였고 각 문자열 필드에 제약조건을 추가하였습니다
    
    Resolve: #10

--- src/main/java/com/adhd/jova_v2/global/alarm/entity/AlarmContent.java
+++ src/main/java/com/adhd/jova_v2/global/alarm/entity/AlarmContent.java
@@ -1,4 +1,4 @@
-package com.adhd.jova_v2.global.alarm.dto;
+package com.adhd.jova_v2.global.alarm.entity;
 
 import com.adhd.jova_v2.global.alarm.enums.AlarmLevel;
 import com.adhd.jova_v2.global.alarm.enums.AlarmStatus;
@@ -19,9 +19,9 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Getter
 public class AlarmContent {
-    @Column(nullable = false)
+    @Column(nullable = false, length = 50)
     private String title;
-    @Column(nullable = false)
+    @Column(nullable = false, length = 255)
     private String content;
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
